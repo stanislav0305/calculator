13448cfd3df8e545ab786ac9820d26a7f0b3cbd6
stanislav0305 <0305stas@inbox.lv>
2019-04-08 21:51:55 +0300

    * file attachment for order email

--- js/libConfig.js
+++ js/libConfig.js
@@ -1,10 +1,21 @@
-var libConfigModule = (function() {
+﻿var libConfigModule = (function() {
     
-    let initJQueryValidation = function() {
-        $.validate({
-            modules: 'html5',
-            decimalSeparator: '.'
+    let initJQueryValidation = function () {
+        /*
+        $.validator.setDefaults({
+            debug: true
         });
+        */
+        jQuery.validator.setDefaults({
+            debug: true,
+            success: "valid",
+            messages: $.validator.messages
+        });
+
+
+        jQuery.validator.addMethod("latvianPhone", function (value, element) {
+            return this.optional(element) || /(^[+]371\d{8}$)|(^371\d{8}$)|(^\d{8}$)/g.test(value);
+        }, "Не верный формат телефона");
     }; 
 
     let initUnderscore = function () {
@@ -16,10 +27,16 @@ var libConfigModule = (function() {
         };
     };
 
+    /*
+    let initBootstapInputMasks = function() {
+        $(":input").inputmask();
+    }*/
+
     let module = {
         init: function () {
             initJQueryValidation();
             initUnderscore();
+           // initBootstapInputMasks();
         }
     };
 
--- js/steps/totalPriceStep.js
+++ js/steps/totalPriceStep.js
@@ -5,7 +5,8 @@
     let calculatorConfig;
     let calcHelper;
 
-    let clientDravingCount = 0;
+    let clientDravingCount = 1;
+    let clientDravingNumber = 0;
 
     let getBase64String = function(inputElement) {
         var deferred = $.Deferred();
@@ -46,12 +47,60 @@
 
     let appendCliendDravingBlock = function() {
         clientDravingCount = clientDravingCount + 1;
-        let clientDravingNumber = clientDravingCount;
+        clientDravingNumber = clientDravingNumber + 1;
+
         let data = {
             number: clientDravingNumber
         };
         let html = templateHelper.getTemplateResult("file-input-block", data);
         $("#file-input-block-container").append(html);
+    };
+
+    let sendOrderEmail = function() {
+        let client = module.getFormData("#sendOrderForm");
+        let totalPrice = $('#totalPrice').text();
+        let log = calcHelper.getLogger().getFiltredLog();
+        let attachments = getAttachments($(".client-drawing"));
+
+        let fileNames = _($(".client-drawing"))
+            .map(function (fileInput) {
+                let fileName = $(fileInput).attr("file-name");
+                return fileName;
+            })
+            .filter(function (fileName) {
+                return (fileName != undefined && fileName !== "");
+            });
+
+
+        let mailData = mailSender.createOrderMailDataObj(client, totalPrice, fileNames, log, attachments);
+
+        $("#sendProgressNotification").fadeIn(2000,
+            function () {
+                let promise = mailSender.send(mailData);
+                promise.then(
+                    message => {
+                        $("#sendProgressNotification").fadeOut(500);
+                        if (message === "OK") {
+                            $("#sendFinishNotification")
+                                .fadeIn(500,
+                                function () {
+                                    module.hideSendForm();
+                                })
+                                .delay(5000)
+                                .fadeOut(2000, "linear");
+                        } else {
+                            console.log(`В процессе отправки произошла ошибка. Ошибка: ${message}`);
+                            $("#sendErrorText").text(message);
+
+                            $("#sendErrorNotification")
+                                .fadeIn(500)
+                                .delay(5000)
+                                .fadeOut(2000, "linear");
+                        }
+
+                    }
+                );
+            });
     };
 
     let module = {
@@ -69,7 +118,8 @@
                 module.setOrderBtnEvent();
                 module.setOrderFormSubmitEvent();
 
-                clientDravingCount = 0;
+                clientDravingCount = 1;
+                clientDravingNumber = 0;
                 module.setClientDravingChangeEvent();
             }
         },
@@ -127,77 +177,90 @@
                         return;
                     }
 
+                    if ($fileInput[0].files[0].size > 25000000) { // 25 Mb
+                        alert("Размер файла не может привышать 25 Мб");
+                        $fileInput.val("");
+                        return;
+                    }
+
                     $fileInput.attr("base64-file-string", base64Data);
                     $fileInput.attr("file-name", fileName);
 
-                    $(`#client-drawing-label-${inputNumber}`).addClass("selected").html(fileName);
+                    $(`#client-drawing-label-${inputNumber}`)
+                        .addClass("selected").html(fileName);
 
-                    appendCliendDravingBlock();
+                    if (clientDravingCount < 10) { // max 10 files
+                        appendCliendDravingBlock();
+                    }
                 });
             });
 
             $(document).on("click", ".file-input-block-remove-btn", function() {
                 let inputNumber = $(this).attr("input-number");
                 $(`#file-input-block-${inputNumber}`).remove();
+                clientDravingCount = clientDravingCount - 1;
+                let count = _.filter($('.client-drawing'),
+                    function(input) {
+                        return !input.value;
+                    }).length;
+
+                if (count === 0) {  // max 10 files
+                    appendCliendDravingBlock();
+                }
             });
     },
         removeClientDravingChangeEvents: function () {
             $(document).off("change", ".client-drawing");
             $(document).off("click", ".file-input-block-remove-btn");
         },
-	    setOrderFormSubmitEvent : function(){
+        setOrderFormSubmitEvent: function () {
+            $("#sendOrderForm").validate({
+                rules: {
+                    name: {
+                        required: true,
+                        rangelength: [2, 50]
+                    },
+                    surname: {
+                        required: true,
+                        rangelength: [2, 50]
+                    },
+                    phone: {
+                        required: true,
+                        latvianPhone: true
+                    }
+                },
+                showErrors: function(errorMap, errorList) {
+                    $(".tooltipe-form-error").remove();
+
+                    _.each(errorList,
+                        function(item) {
+                            let errorContainerId = "tooltipe-form-error-container-id-" + item.element.name;
+                            let errorLabelId = "tooltipe-form-error-lable-id-" + item.element.name;
+
+                            if ($(`#${errorContainerId}`).length === 0) {
+                                let data = {
+                                    containerId: errorContainerId,
+                                    labelId: errorLabelId,
+                                    errorMessage: item.message
+                                };
+                                let html = templateHelper.getTemplateResult("tooltipe-form-error", data);
+                                $(item.element).parent().append(html);
+                            } else {
+                                $(`#${errorLabelId}`).html(item.message);
+                            }
+                        });
+                }
+        }); 
+            
             $(document).on('submit', "#sendOrderForm", function (event) {
                 event.preventDefault();
 
-                //$("#sendOrderForm").validate();
+                if ($("#sendOrderForm").valid() === true) {
+                    sendOrderEmail();
+                }
 
-                let client = module.getFormData("#sendOrderForm");
-                let totalPrice = $('#totalPrice').text();
-                let log = calcHelper.getLogger().getFiltredLog();
-                let attachments = getAttachments($(".client-drawing"));
-
-                let fileNames = _($(".client-drawing"))
-                    .map(function(fileInput) {
-                        let fileName = $(fileInput).attr("file-name");
-                        return fileName;
-                    })
-                    .filter(function(fileName) {
-                        return (fileName != undefined && fileName !== "");
-                    });
-               
-                
-                let mailData = mailSender.createOrderMailDataObj(client, totalPrice, fileNames, log, attachments);
-                
-                $("#sendProgressNotification").fadeIn(2000,
-                    function() {
-                        let promise = mailSender.send(mailData);
-                        promise.then(
-                            message => {
-                                $("#sendProgressNotification").fadeOut(500);
-                                if (message === "OK") {
-                                    $("#sendFinishNotification")
-                                        .fadeIn(500,
-                                            function() {
-                                                module.hideSendForm();
-                                            })
-                                        .delay(5000)
-                                        .fadeOut(2000, "linear");
-                                } else {
-                                    console.log(`В процессе отправки произошла ошибка. Ошибка: ${message}`);
-                                    $("#sendErrorText").text(message);
-
-                                    $("#sendErrorNotification")
-                                        .fadeIn(500)
-                                        .delay(5000)
-                                        .fadeOut(2000, "linear");
-                                }
-
-                            }
-                        );
-                    });
-                
                 return false;
-            });
+            }); 
         },
         removeOrderFormSubmitEvent: function () {
             $(document).off('submit', "#sendOrderForm");
