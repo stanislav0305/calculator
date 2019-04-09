2306f856450c91f7811b7dc93161476b8d8b7d54
stanislav0305 <0305stas@inbox.lv>
2019-04-09 09:54:01 +0300

    * Added email total attachnets size check

--- js/steps/totalPriceStep.js
+++ js/steps/totalPriceStep.js
@@ -6,7 +6,10 @@
     let calcHelper;
 
     let clientDravingCount = 1;
+    const CLIENT_DRAVING_MAX_COUNT = 10;
     let clientDravingNumber = 0;
+    let clientDravingTotalFilesSizeInBytes = 0;
+    const CLIENT_DRAVING_MAX_TOTAL_FILES_SIZE_IN_BYTES = 25 * 1000000; // 25 Mb
 
     let getBase64String = function(inputElement) {
         var deferred = $.Deferred();
@@ -24,6 +27,16 @@
 
         return deferred.promise();
     };
+
+    let getFileSize = function ($fileInput) {
+        var f = $fileInput.files[0];
+
+        if (f.size !== undefined) {
+            return f.size;
+        }
+
+        return f.fileSize;
+    }
 
     let getAttachmentObj = function($fileInput) {
         return {
@@ -103,6 +116,8 @@
             });
     };
 
+    
+
     let module = {
         totalPriceBlock: function(element, stepId) {
             if (element.totalPriceBlock !== undefined) {
@@ -164,32 +179,56 @@
            
             $(document).on("change", ".client-drawing", function() {
                 let $fileInput = $(this);
+                let fileName = $fileInput.val().split("\\").pop();
+
+                var idxDot = fileName.lastIndexOf(".") + 1;
+                var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+                if (extFile !== "jpg" && extFile !== "jpeg" && extFile !== "pdf") {
+                    alert("Можно загружать только jpg/jpeg или pdf файлы!");
+                    $fileInput.val("");
+                    return;
+                }
+
+                let fileSize = getFileSize($fileInput[0]);
+
+                if (fileSize > CLIENT_DRAVING_MAX_TOTAL_FILES_SIZE_IN_BYTES) {
+                    alert(`Размер файла не может привышать ${CLIENT_DRAVING_MAX_TOTAL_FILES_SIZE_IN_BYTES / 1000000} Мб`);
+                    $fileInput.val("");
+                    return;
+                }
+
+                if (clientDravingTotalFilesSizeInBytes + fileSize > CLIENT_DRAVING_MAX_TOTAL_FILES_SIZE_IN_BYTES) {
+                    alert(`Общий объём приложенных файлов не может привышать ${CLIENT_DRAVING_MAX_TOTAL_FILES_SIZE_IN_BYTES / 1000000} Мб`);
+                    $fileInput.val("");
+                    return;
+                }
+
+                //if replace file
+                let currentFileSize = $fileInput.attr("file-size-in-bytes");
+                if (currentFileSize === undefined) {
+                    currentFileSize = 0;
+                }
+                clientDravingTotalFilesSizeInBytes = clientDravingTotalFilesSizeInBytes - currentFileSize;
+                $fileInput.attr("file-size-in-bytes", 0);
+
                 getBase64String($fileInput).done(function (base64Data) {
                     let inputNumber = $fileInput.attr("input-number");
-                    let fileName = $fileInput.val().split("\\").pop();
                     fileName = `${inputNumber}-${fileName}`;
-
-                    var idxDot = fileName.lastIndexOf(".") + 1;
-                    var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
-                    if (extFile !== "jpg"  && extFile !== "jpeg" && extFile !== "pdf") {
-                        alert("Можно загружать только jpg/jpeg или pdf файлы!");
-                        $fileInput.val("");
-                        return;
-                    }
-
-                    if ($fileInput[0].files[0].size > 25000000) { // 25 Mb
-                        alert("Размер файла не может привышать 25 Мб");
-                        $fileInput.val("");
-                        return;
-                    }
 
                     $fileInput.attr("base64-file-string", base64Data);
                     $fileInput.attr("file-name", fileName);
+                    $fileInput.attr("file-size-in-bytes", fileSize);
+                    clientDravingTotalFilesSizeInBytes = clientDravingTotalFilesSizeInBytes + fileSize;
 
                     $(`#client-drawing-label-${inputNumber}`)
                         .addClass("selected").html(fileName);
 
-                    if (clientDravingCount < 10) { // max 10 files
+                    let emptyInputCount = _.filter($('.client-drawing'),
+                        function (input) {
+                            return !input.value;
+                        }).length;
+
+                    if (clientDravingCount < CLIENT_DRAVING_MAX_COUNT && emptyInputCount === 0) {
                         appendCliendDravingBlock();
                     }
                 });
@@ -197,18 +236,42 @@
 
             $(document).on("click", ".file-input-block-remove-btn", function() {
                 let inputNumber = $(this).attr("input-number");
+
+                let fileSize = $(`#client-drawing-${inputNumber}`).attr("file-size-in-bytes");
+                if (fileSize === undefined) {
+                    fileSize = 0;
+                }
+
                 $(`#file-input-block-${inputNumber}`).remove();
                 clientDravingCount = clientDravingCount - 1;
-                let count = _.filter($('.client-drawing'),
+                clientDravingTotalFilesSizeInBytes = clientDravingTotalFilesSizeInBytes - fileSize;
+
+                let emptyInputCount = _.filter($('.client-drawing'),
                     function(input) {
                         return !input.value;
                     }).length;
 
-                if (count === 0) {  // max 10 files
+                if (emptyInputCount === 0) {  // max 10 files
                     appendCliendDravingBlock();
                 }
             });
-    },
+
+            $(document).on("click", ".file-input-block-clear-btn", function () {
+                let inputNumber = $(this).attr("input-number");
+
+                let fileSize = $(`#client-drawing-${inputNumber}`).attr("file-size-in-bytes");
+                if (fileSize === undefined) {
+                    fileSize = 0;
+                }
+                clientDravingTotalFilesSizeInBytes = clientDravingTotalFilesSizeInBytes - fileSize;
+
+                $(`#client-drawing-${inputNumber}`).val("");
+                $(`#client-drawing-${inputNumber}`).attr("base64-file-string", "");
+                $(`#client-drawing-${inputNumber}`).attr("file-name", "");
+                $(`#client-drawing-${inputNumber}`).attr("file-size-in-bytes", 0);
+                $(`#client-drawing-label-${inputNumber}`).text("Выбрать файл .jpg .jpeg .pdf");
+            });
+        },
         removeClientDravingChangeEvents: function () {
             $(document).off("change", ".client-drawing");
             $(document).off("click", ".file-input-block-remove-btn");
